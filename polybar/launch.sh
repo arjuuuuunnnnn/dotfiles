31890f682c5b616b05bd4d604311f2ecd5d9c8cc
arjuuuuunnnnn <arjunbanur27@gmail.com>
2024-03-27 19:36:23 +0530

    polybar

---
+++ polybar/launch.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+#killall -q polybar
+
+# Wait until the processes have been shut down
+#while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar1 and bar2
+#polybar example
+
+
+#!/bin/sh
+
+# Terminate already running bar instances
+killall -q i3bar
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch polybar, using default config location ~./config/polybar/config
+polybar main &
+
+echo "Bars launched..."
+
