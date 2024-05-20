491e3aeaae224561c4b8d4734823874fe1f9092b
arjuuuuunnnnn <arjunbanur27@gmail.com>
2024-05-20 14:53:19 +0530

    icon

--- .config/polybar/polybar-scripts/powermenu.sh
+++ .config/polybar/polybar-scripts/powermenu.sh
@@ -4,9 +4,9 @@
 chosen=$(printf " Power Off\n Reboot\n Lock" | rofi -dmenu -i -theme-str '@import "~/.config/rofi/power.rasi"')
 
 case "$chosen" in
-	"Power Off") poweroff ;;
-	"Reboot") reboot ;;
-	"Lock") i3lock ;;
+	" Power Off") poweroff ;;
+	" Reboot") reboot ;;
+	" Lock") i3lock ;;
 	*) exit 1 ;;
 esac
 
