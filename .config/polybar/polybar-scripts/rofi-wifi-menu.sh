949028e3873618bdb60c6a91d868f034fec88431
arjuuuuunnnnn <arjunbanur27@gmail.com>
2024-06-09 10:55:27 +0530

    edit: wifi menu

--- .config/polybar/polybar-scripts/rofi-wifi-menu.sh
+++ .config/polybar/polybar-scripts/rofi-wifi-menu.sh
@@ -24,11 +24,12 @@ else
 	# Get saved connections
 	saved_connections=$(nmcli -g NAME connection)
 	if [[ $(echo "$saved_connections" | grep -w "$chosen_id") = "$chosen_id" ]]; then
-		nmcli connection up id "$chosen_id" | grep "successfully" && notify-send "Connection Established" "$success_message"
+		nmcli connection up id "$chosen_id" | grep "successfully" && notify-send "$success_message"
 	else
 		if [[ "$chosen_network" =~ "" ]]; then
 			wifi_password=$(rofi -dmenu -p "Password: " )
 		fi
-		nmcli device wifi connect "$chosen_id" password "$wifi_password" | grep "successfully" && notify-send "Connection Established" "$success_message"
+		nmcli device wifi connect "$chosen_id" password "$wifi_password" | grep "successfully" && notify-send "$success_message"
     fi
 fi
+
